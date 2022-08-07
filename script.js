29ebe3c1bca9fb5e72af6cceda2d50a430838186
ConwayCJ <conwaycj@plu.edu>
2022-08-06 20:03:59 -0700

    Reformat code

--- script.js
+++ script.js
@@ -5,37 +5,39 @@
 4. 
 */
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-
 let submitButton = document.getElementById('submitButton')
-let tbody = document.querySelector("#main-table tbody")
+let tbody = document.querySelector('#main-table tbody')
 
-
-function addNewRow (event) {
+function addNewRow(event) {
   event.preventDefault()
   let row = document.createElement('tr')
-  
-  let tdNumber = document.createElement("td")
-  let noLength = tbody.children.length +1
+
+  let tdNumber = document.createElement('td')
+  let noLength = tbody.children.length + 1
   tdNumber.innerText = noLength
   row.appendChild(tdNumber)
-  
-  let tdTask = document.createElement("td")
+
+  let tdTask = document.createElement('td')
   tdTask.classList.add('task')
-  tdTask.innerText = capitalizeFirstLetter(document.querySelector('#userInput').value)
+  tdTask.innerText = capitalizeFirstLetter(
+    document.querySelector('#userInput').value
+  )
   document.querySelector('#userInput').value = null
   row.appendChild(tdTask)
-  
-  let tdCompleted = document.createElement("td")
-  tdCompleted.appendChild(document.querySelector('label.switch').cloneNode(true))
+
+  let tdCompleted = document.createElement('td')
+  tdCompleted.appendChild(
+    document.querySelector('label.switch').cloneNode(true)
+  )
   tdCompleted.classList.add('text-center')
   row.appendChild(tdCompleted)
- 
-  let tdActions = document.createElement("td")
+
+  let tdActions = document.createElement('td')
   let newButton = document.createElement('button')
-  newButton.className = "del-button"
+  newButton.className = 'del-button'
   let iEle = document.createElement('i')
   iEle.className = 'fa fa-trash-o'
   newButton.appendChild(iEle)
@@ -47,16 +49,11 @@ function addNewRow (event) {
   tbody.appendChild(row)
 }
 
-
-
-
-function deleteRow (event) {
-
+function deleteRow(event) {
   if (confirm('This row is about to be deleted.')) {
     event.currentTarget.closest('tr').remove()
   }
 }
-
 
 let newItemForm = document.querySelector('#new-row')
 
