ecac0abddc27a317ec1056710fc6b78f00f51a23
ConwayCJ <conwaycj@plu.edu>
2022-08-13 22:58:55 -0700

    Finish tic-tac-toe

--- script.js
+++ script.js
@@ -1,60 +1,125 @@
-/*TODO LIST:
-1. Make submit button add new row
-2. Make submit button take in information to add new row
-3. Make delete button on the right work correctly
-4. 
-*/
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
+let currentPlayer
+let turnCount
+const gameBoard = [
+    '', '', '',
+    '', '', '',
+    '', '', '',
+]
+/*
+    Board Indexes
+
+    [0], [1], [2],
+    [3], [4], [5],
+    [6], [7], [8], */
+
+function changePlayer () {
+    if (currentPlayer == 'X') {
+        currentPlayer = 'O'
+        $('.turn').text(`Player 2's Turn!`)
+    } else {
+        currentPlayer = 'X'
+        $('.turn').text(`Player 1's Turn!`)
+    }
 }
 
-let submitButton = document.getElementById('submitButton')
-let tbody = document.querySelector('#main-table tbody')
 
-function addNewRow(event) {
-  event.preventDefault()
-  let row = document.createElement('tr')
+const handlePlayerMove = function(event) {
+    let gameWon = false
+    const box = event.target
+    if (box.innerHTML == '') {
+        box.innerHTML = currentPlayer
+    } else {
+        return
+    }
+    turnCount++
+    let boxIndex = box.getAttribute('data-cell-index')
+    gameBoard[boxIndex] = currentPlayer
+    if(checkWinner()) {
+        gameWon = true
+        $('.turn').text(`${currentPlayer} wins!`)
+        removeListeners()
+        return
+    }
 
-  let tdNumber = document.createElement('td')
-  let noLength = tbody.children.length + 1
-  tdNumber.innerText = noLength
-  row.appendChild(tdNumber)
+    console.log(turnCount,gameWon)
+    if (turnCount === 9 && gameWon == false) {
+        $('.turn').text('Tie!')
+        removeListeners()
+        return
+    }
 
-  let tdTask = document.createElement('td')
-  tdTask.classList.add('task')
-  tdTask.innerText = capitalizeFirstLetter(
-    document.querySelector('#userInput').value
-  )
-  document.querySelector('#userInput').value = null
-  row.appendChild(tdTask)
-
-  let tdCompleted = document.createElement('td')
-  tdCompleted.appendChild(
-    document.querySelector('label.switch').cloneNode(true)
-  )
-  tdCompleted.classList.add('text-center')
-  row.appendChild(tdCompleted)
-
-  let tdActions = document.createElement('td')
-  let newButton = document.createElement('button')
-  newButton.className = 'del-button'
-  let iEle = document.createElement('i')
-  iEle.className = 'fa fa-trash-o'
-  newButton.appendChild(iEle)
-
-  newButton.addEventListener('click', deleteRow)
-  tdActions.appendChild(newButton)
-  row.appendChild(tdActions)
-
-  tbody.appendChild(row)
+    changePlayer()
 }
 
-function deleteRow(event) {
-  if (confirm('This row is about to be deleted.')) {
-    event.currentTarget.closest('tr').remove()
-  }
+$('.board').on('click', handlePlayerMove)
+
+function removeListeners () {
+    $('.board').off()
 }
 
-let newItemForm = document.querySelector('#new-row')
 
-newItemForm.addEventListener('submit', addNewRow)
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+ function checkWinner() {
+    let playerWon = false;
+
+    winningCombinations.forEach((combination) => {
+        if (playerWon) {
+            return;
+        }
+       //For each iteration, take each value from our combination
+       //and compare to see if THAT position on our gameBoard
+       // is == to currentPlayer       
+       playerWon = combination.every(index => gameBoard[index] === currentPlayer);
+        
+    })
+    console.log(playerWon)
+    return playerWon;
+}
+
+function startGame() {
+    $('.board').on('click', handlePlayerMove)
+    currentPlayer = 'X'
+    $('.turn').text(`Player 1's Turn!`)
+    turnCount = 0
+}
+
+$('.resetButton').on('click', function () {
+    $('.box').empty()
+    startGame()
+    gameBoard.fill('')
+})
+
+startGame()
+
+
+
+
+
+
+// function checkWinner() {
+
+
+
+//     for (let i = 0; i < winningCombinations.length; i++) {
+
+
+
+//         winningCombinations[i].every((ele) => {
+            
+//             return gameBoard[ele] == 'X'
+//         })
+//         }
+//     }
+    
+
+
